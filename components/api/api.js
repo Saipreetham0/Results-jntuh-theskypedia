b6db683a1a4c941676431d2d1a92c267828b46c7
Saipreetham <39019648+Saipreetham0@users.noreply.github.com>
2023-09-03 19:42:32 +0530

    Api Update

--- components/api/api.js
+++ components/api/api.js
@@ -1,6 +1,12 @@
+// const urll = "https://jntuhresults.up.railway.app"
+// const urlQ = "http://127.0.0.1:8000/"
+// const url = "https://resultsjntuhv2.netlify.app"
+// const urlr = "https://jntuhresults.herokuapp.com"
+// const urlrr = "https://jntuhresults.up.railway.app"
+
 const urll = "https://jntuhresults.up.railway.app"
 const urlQ = "http://127.0.0.1:8000/"
-const url = "https://resultsjntuhv2.netlify.app"
+const url = "https://resultsjntuhv5.netlify.app"
 const urlr = "https://jntuhresults.herokuapp.com"
 const urlrr = "https://jntuhresults.up.railway.app"
 // const url = "https://jntuhresults.up.railway.app"
--- components/api/api2.js
+++ components/api/api2.js
@@ -1,3 +1,5 @@
-const redisurl = "https://resultsjntuhv4.netlify.app"
+// const redisurl = "https://resultsjntuhv4.netlify.app"
+const redisurl = "https://resultsjntuhv6.netlify.app"
+
 
 export default redisurl;
